a2a8ed579ea4c052054180b2b6e89146925b9379
Per A. Bergersen <pab2222@hotmail.com>
2025-01-08 13:15:53 +0100

    added table user

--- FileUpload/Default.aspx
+++ FileUpload/Default.aspx
@@ -7,14 +7,20 @@
     <title></title>
 </head>
 <body>
+
+    <h1>uke 37 plakat</h1>
+
+    ditt bilde her<br />
+    <img height="300" src="mittbilde.jpg" />
+
     <form id="form1" runat="server">
-        <asp:DataList ID="DataList1" runat="server" OnItemCommand="DataList1_ItemCommand">
-            <ItemTemplate>
-                <img height="300" src="<%# Container.DataItem %>" />
-                <br />
-                <asp:Button ID="ButtonDelete" CommandName="<%# Container.DataItem %>" runat="server" Text="Delete" />
-            </ItemTemplate>
-        </asp:DataList>
+            <asp:DataList ID="DataList1" runat="server" OnItemCommand="DataList1_ItemCommand">
+                <ItemTemplate>
+                    <img height="300" src="<%# Container.DataItem %>" />
+                    <br />
+                    <asp:Button ID="ButtonDelete" CommandName="<%# Container.DataItem %>" runat="server" Text="Delete" />
+                </ItemTemplate>
+            </asp:DataList>
     </form>
 </body>
 </html>
--- FileUpload/Login.aspx.cs
+++ FileUpload/Login.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,12 +19,43 @@ namespace FileUpload
 
         protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
         {
-            if (Login1.Password == "123")
+            //her kommer det en query
+            //select
+
+            //hente ut un og pw fra login
+            string userName = Login1.UserName;
+            string passWord=Login1.Password;
+
+            bool ok=GetUserByUserNameAndPassword(userName, passWord);
+
+            if (ok)
             {
                 e.Authenticated = true;
                 Login1.Visible = false;
                 
             }
+        }
+
+        public bool GetUserByUserNameAndPassword(string user, string passWord)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnAir"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * from [User] where UN=@un and PW=@pw", conn);
+                cmd.CommandType = CommandType.Text;
+
+                cmd.Parameters.Add(new SqlParameter("@un",user ));
+                cmd.Parameters.Add(new SqlParameter("@pw", passWord));
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                    return true;
+                reader.Close();
+                conn.Close();
+            }
+            return false;
         }
     }
 }
